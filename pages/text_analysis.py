49194c6fec2d9f04233818967a0169e333e9eaae
kushalsourav <66814079+kushalsourav@users.noreply.github.com>
2024-04-16 10:13:01 +0530

    Update text_analysis.py

--- pages/text_analysis.py
+++ pages/text_analysis.py
@@ -1,7 +1,5 @@
 import streamlit as st
 import pandas as pd
-import matplotlib.pyplot as plt
-from wordcloud import WordCloud
 from sklearn.feature_extraction.text import TfidfVectorizer
 from sklearn.metrics.pairwise import cosine_similarity
 from nltk.corpus import stopwords
